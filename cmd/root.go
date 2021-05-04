e0a5fa484bc5859284fc596a34be7c83c7bb668e
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-05-04 19:57:44 +0500

    add root command

--- cmd/root.go
+++ cmd/root.go
@@ -28,4 +28,53 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
+
+var (
+	configPath string
+	debug bool
+	v *viper.Viper
+	log logger.Logger
 )
+
+const prefix = 'cmd.root'
+
+// Root command
+var rootCmd = &cobra.Command{
+	Use: "bin [command]",
+	Long: "",
+	SilenceUsage: true,
+	SilenceErrors: true,
+	PersistentPreRun: func(cmd *cobra.Command, _[]string) {
+		l, c, e := logger.Build()
+		defer c()
+		if e != nil {
+			panic(e)
+		}
+
+		log = l.WithFields(logger.Fields{"service": prefix})
+
+		v.SetConfigFile(configPath)
+
+		if configPath != "" {
+			e := v.ReadInConfig()
+			if e != nil {
+				log.Error("can't read config, %v", logger.Args(errors.WithMessage(e, prefix)))
+				os.Exit(1)
+			}
+		}
+
+		if debug {
+			b, _ := json.Marshal(v.AllSettings())
+			var out bytes.Buffer
+			e := json.Indent(&out, b, "", "  ")
+			if e != nil {
+				log.Error("can't prettify config")
+				os.Exit(1)
+			}
+			fmt.Println(string(out.Bytes()))
+		}
+
+		_, _ = maxprocs.Set(maxprocs.Logger(log.Printf))
+	},
+}
+
